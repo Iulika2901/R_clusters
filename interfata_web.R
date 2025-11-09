c28c7f76e046c252d5ce8d92fed2cc97f69b75a6
unknown <bacriu.iuliana@yahoo.com>
2025-11-09 17:29:16 +0200

    all clusters method printed

--- interfata_web.R
+++ interfata_web.R
@@ -7,13 +7,18 @@ library(bslib)
 library(igraph)
 
 ui <- page_sidebar(
-  title = "K-Means pe Graf",
+  title = "Graph Clustering Demo",
   
   sidebar = sidebar(
-    numericInput("k", "Număr de clustere (k):", min = 1, max = 10, value = 3),
     fileInput("file", "Alege fișierul cu matricea de adiacență:", 
               accept = c(".csv", ".R", ".RData", ".rds")),
-    actionButton("run", "Rulează K-means")
+    numericInput("k", "Număr de clustere (pentru k-means):", min = 1, max = 10, value = 3),
+    actionButton("btn_kmeans", "K-Means"),
+    actionButton("btn_fastgreedy", "Fast Greedy"),
+    actionButton("btn_louvain", "Louvain"),
+    actionButton("btn_walktrap", "Walktrap"),
+    actionButton("btn_label", "Label Propagation"),
+    actionButton("btn_infomap", "Infomap")
   ),
   
   mainPanel(
@@ -24,90 +29,126 @@ ui <- page_sidebar(
 
 server <- function(input, output, session) {
   
+ 
   data_reactive <- reactive({
     infile <- input$file
-    if (is.null(infile)) {
-      if (file.exists("adjacency.csv")) {
-        read.csv("adjacency.csv", row.names = 1)
-      } else {
-        stop("Nu s-a încărcat fișier și nu există 'adjacency.csv' în folderul curent.")
-      }
-    } else {
-      ext <- tools::file_ext(infile$name)
-      if (ext == "csv") read.csv(infile$datapath, row.names = 1)
-      else if (ext == "rds") readRDS(infile$datapath)
-      else if (ext %in% c("R", "r")) {
-        source(infile$datapath, local = TRUE)
-        df <- Filter(is.data.frame, mget(ls(), inherits = TRUE))
-        if (length(df) > 0) df[[1]] else stop("Fișierul .R nu conține un data.frame!")
-      } 
-      else if (ext == "RData") {
-        temp_env <- new.env()
-        load(infile$datapath, envir = temp_env)
-        objs <- mget(ls(temp_env), envir = temp_env)
-        df <- NULL
-        for (o in objs) {
-          if (is.data.frame(o) || is.matrix(o)) {
-            df <- as.data.frame(o)
-            break
-          }
+    req(infile)
+    
+    ext <- tools::file_ext(infile$name)
+    if (ext == "csv") read.csv(infile$datapath, row.names = 1)
+    else if (ext == "rds") readRDS(infile$datapath)
+    else if (ext %in% c("R", "r")) {
+      source(infile$datapath, local = TRUE)
+      df <- Filter(is.data.frame, mget(ls(), inherits = TRUE))
+      if (length(df) > 0) df[[1]] else stop("Fișierul .R nu conține un data.frame!")
+    } 
+    else if (ext == "RData") {
+      temp_env <- new.env()
+      load(infile$datapath, envir = temp_env)
+      objs <- mget(ls(temp_env), envir = temp_env)
+      df <- NULL
+      for (o in objs) {
+        if (is.data.frame(o) || is.matrix(o)) {
+          df <- as.data.frame(o)
+          break
         }
-        if (is.null(df)) stop("Fișierul .RData nu conține un data.frame sau matrice!")
-        df
       }
+      if (is.null(df)) stop("Fișierul .RData nu conține un data.frame sau matrice!")
+      df
     }
   })
   
-  clusters <- eventReactive(input$run, {
-    adj <- data_reactive()
+
+  run_clustering <- function(g, method, k = 3) {
+    if (method == "kmeans") {
+      layout <- layout_with_fr(g)
+      km <- kmeans(layout, centers = k)
+      list(layout = layout, cluster = km$cluster, centers = km$centers)
+    } else {
     
-    # Verificăm dimensiunea
-    if (!is.matrix(adj) && !is.data.frame(adj)) stop("Datele trebuie să fie matrice/pseudo-matrice.")
-    adj <- as.matrix(adj)
-    
-    if (nrow(adj) < 2) {
-      showNotification("⚠️ Matricea de adiacență are prea puține noduri!", type = "error")
-      return(NULL)
+      comm <- switch(method,
+                     fastgreedy = cluster_fast_greedy(g),
+                     louvain = cluster_louvain(g),
+                     walktrap = cluster_walktrap(g),
+                     label = cluster_label_prop(g),
+                     infomap = cluster_infomap(g))
+      cluster <- membership(comm)
+      layout <- layout_with_fr(g)
+      list(layout = layout, cluster = cluster, centers = NULL)
     }
-    
+  }
+  
+
+  clustering_res <- reactiveVal(NULL)
+  
+
+  observeEvent(input$btn_kmeans, {
+    adj <- as.matrix(data_reactive())
     g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
-    layout <- layout_with_fr(g)
-    
-    k <- input$k
-    n <- nrow(layout)
-    if (k > n) {
-      showNotification(paste("⚠️ k =", k, "este mai mare decât numărul de noduri (", n, "). Ajustez automat."), 
-                       type = "warning")
-      k <- n
-    }
-    
-    km <- kmeans(layout, centers = k)
-    
-    list(graph = g, layout = layout, km = km)
+    res <- run_clustering(g, "kmeans", input$k)
+    res$graph <- g
+    clustering_res(res)
   })
   
-  # Plot grafic
+  observeEvent(input$btn_fastgreedy, {
+    adj <- as.matrix(data_reactive())
+    g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+    res <- run_clustering(g, "fastgreedy")
+    res$graph <- g
+    clustering_res(res)
+  })
+  
+  observeEvent(input$btn_louvain, {
+    adj <- as.matrix(data_reactive())
+    g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+    res <- run_clustering(g, "louvain")
+    res$graph <- g
+    clustering_res(res)
+  })
+  
+  observeEvent(input$btn_walktrap, {
+    adj <- as.matrix(data_reactive())
+    g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+    res <- run_clustering(g, "walktrap")
+    res$graph <- g
+    clustering_res(res)
+  })
+  
+  observeEvent(input$btn_label, {
+    adj <- as.matrix(data_reactive())
+    g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+    res <- run_clustering(g, "label")
+    res$graph <- g
+    clustering_res(res)
+  })
+  
+  observeEvent(input$btn_infomap, {
+    adj <- as.matrix(data_reactive())
+    g <- graph_from_adjacency_matrix(adj, mode = "undirected", weighted = TRUE)
+    res <- run_clustering(g, "infomap")
+    res$graph <- g
+    clustering_res(res)
+  })
+  
+
+  plot_graph <- function(g, layout, cluster, centers = NULL, k = 3) {
+    plot(g, layout = layout, vertex.color = cluster, vertex.size = 15,
+         vertex.label = NA, main = paste("Graph Clustering"))
+    if (!is.null(centers)) points(centers, col = 1:k, pch = 8, cex = 2, lwd = 2)
+  }
+  
+
   output$graphPlot <- renderPlot({
-    res <- clusters()
+    res <- clustering_res()
     req(!is.null(res))
-    
-    g <- res$graph
-    layout <- res$layout
-    km <- res$km
-    
-    # Plot noduri colorate
-    plot(g, layout = layout, vertex.color = km$cluster, vertex.size = 15,
-         vertex.label = NA, main = paste("K-Means pe Graf (k =", input$k, ")"))
-    
-    # Adaugă stele pentru centrele clusterelor
-    points(km$centers, col = 1:input$k, pch = 8, cex = 2, lwd = 2)
+    plot_graph(res$graph, res$layout, res$cluster, res$centers, input$k)
   })
   
-  # Tabel cu noduri + cluster
+
   output$clusteredTable <- renderTable({
-    res <- clusters()
+    res <- clustering_res()
     req(!is.null(res))
-    data.frame(Node = rownames(res$graph[]), Cluster = res$km$cluster)
+    data.frame(Node = rownames(res$graph[]), Cluster = res$cluster)
   })
   
 }
